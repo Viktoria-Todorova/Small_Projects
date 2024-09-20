08200b96a55c2a14b7ece5bdacbc4067cfdc0131
Viktoria Todorova <viktoria.todorova.1997@gmail.com>
2024-09-20 17:26:28 +0300

    Update Rock_paper_scissors.py

--- Rock_paper_scissors/Rock_paper_scissors.py
+++ Rock_paper_scissors/Rock_paper_scissors.py
@@ -1,4 +1,4 @@
-import random
+import random  # it generates random numbers or making random choices
 
 rock = 'Rock'
 paper = 'Paper'
@@ -8,8 +8,9 @@ count_comp_wins = 0
 count_mine_wins = 0
 count_comp_loses = 0
 count_mine_loses = 0
-
+# i created a loop so it can be played as much as the player wants
 while True:
+    # giving the player to chose one of the 3 choices
     player_move = input("Choose [r]ock, [p]aper, or [s]cissors: ")
 
     if player_move == "r":
@@ -19,11 +20,12 @@ while True:
     elif player_move == "s":
         player_move = scissors
     else:
-        print(colored("Invalid Input, Try again.."))
+        print(colored("Invalid Input, Try again..")) # raise an exception and exit the program
         continue
 
-    computer_random = random.randint(1, 3)
+    computer_random = random.randint(1, 3) # is used to generate a random integer within a specific range
     computer_move = ""
+    # assign values to the random numbers we generated
     if computer_random == 1:
         computer_move = rock
     elif computer_random == 2:
@@ -32,7 +34,7 @@ while True:
         computer_move = scissors
 
     print(f"The computer chose {computer_move}.")
-
+    # checking the probabilities for win/lose or draw
     if (player_move == rock and computer_move == scissors) or \
        (player_move == paper and computer_move == rock) or \
        (player_move == scissors and computer_move == paper):
@@ -47,7 +49,7 @@ while True:
         print("You lose!")
 
     want_more = input("Do you want to play again? [yes/no]: ")
-
+     # if the player wants to stop the game we give him the choice to stop it and give a result from the game
     if want_more == 'no':
         if count_mine_wins > count_comp_wins:
             print('It was a good game!')
@@ -56,5 +58,6 @@ while True:
             print('It was a good game, but unfortunately you lost!')
             print(f"The computer won with {count_comp_wins} wins and {count_comp_loses} losses")
         break
+        # the program checks the posibility of the player to chose something different from yes or no
     elif want_more != "yes":
         print("Invalid Input, Try again..")
